183db31af2b12e52233782f9c97669c26322f439
Kaustubh Keny <Kaustubh.Keny@Cogencis.com>
2025-08-07 10:26:57 +0530

    changes made

---
+++ app/utils.py
@@ -0,0 +1,194 @@
+import os, re, json, json5, string, shutil, inspect
+import pandas as pd #type:ignore
+from typing import List
+
+
+class Helper:
+    
+    def __init__(self):
+        pass
+    #PARSING UTILS
+    def get_xlsx_in_folder(self,path:str) -> dict:
+        df = pd.DataFrame()
+        for root,_,files in os.walk(path):
+            for file_name in files:
+                if file_name.endswith(".xlsx") and file_name.lower() == "table_data.xlsx":
+                    self.logger.info(f"Excel sheet containing table data found.")
+                    full_path = os.path.join(root, file_name)
+                    df = pd.read_excel(full_path)
+                    return df
+        self.logger.warning(f" 'table_data.xlsx' not found !! Returning empty df.")
+        return df
+
+
+    @staticmethod
+    def copy_pdfs_to_folder(dest_folder: str, data):
+        os.makedirs(dest_folder, exist_ok=True)
+
+        if isinstance(data, dict):
+            file_paths = list(data.values())
+        elif isinstance(data, list):
+            file_paths = data
+        else:
+            raise ValueError("Data must be a list of paths or a dict with path values")
+
+        for path in file_paths:
+            if not os.path.isfile(path):
+                continue
+            try:
+                file_name = os.path.basename(path)
+                dest_path = os.path.join(dest_folder, file_name)
+                shutil.copy2(path, dest_path)
+            except Exception as e:
+                # logger = get_logger()
+                # logger.error(f"Failed to copy '{path}' → {dest_folder}: {e}")
+                pass
+    
+    @staticmethod
+    def delete_files_and_empty_folder(file_path: str) -> bool:
+        try:
+            # print(file_path)
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                
+                parent_dir = os.path.dirname(file_path)
+                print("Remaining:", os.listdir(parent_dir))
+
+                if os.path.isdir(parent_dir) and not os.listdir(parent_dir):
+                    os.rmdir(parent_dir)
+                return True
+            return False
+        except Exception as e:
+            # logger = get_logger()
+            # logger.exception(f"delete_file_and_empty_folder -> {e}")
+            return False
+        
+    @staticmethod
+    def delete_amc_pdf(data):
+        try:
+            for k, path in data.items():
+                Helper.delete_files_and_empty_folder(path)
+        except Exception as e:
+            # logger = get_logger()
+            # logger.exception(f"delete_amc_pdf: {e}")
+            return
+        return
+    
+    #JSON UN/LOAD 
+    @staticmethod
+    def save_json(data: dict,path: str, indent: int = 2,typ = "json"):
+        with open(path, "w", encoding="utf-8") as f:
+            json5.dump(data, f, indent=indent) if typ == "json5" else json.dump(data, f, indent=indent)
+
+    @staticmethod
+    def load_json(path: str,typ = "json"):
+        with open(path, "r", encoding="utf-8") as f:
+            return json5.load(f) if typ == "json5" else json.load(f)
+    
+    #WRITE TEXT
+    @staticmethod
+    def save_text(data,path:str):
+        if not data:
+            print("Empty Data")
+            return
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+        with open(path, 'a', encoding='utf-8') as f:
+            if isinstance(data,dict):
+                f.writelines(f"{k}:{v}\n" for k,v in data.items())
+            elif isinstance(data,list):
+                f.writelines(f"{k}\n" for k in data)
+            elif isinstance(data,str):
+                f.writelines(data)
+            else: print("Invalid type")
+     
+    @staticmethod        
+    def create_dirs(root_path: str, dirs: List[str]) -> List[str]:
+        created_paths = []
+        for dir_name in dirs:
+            full_path = os.path.join(root_path, dir_name)
+            os.makedirs(full_path, exist_ok=True)
+            created_paths.append(full_path)
+        return created_paths
+
+    @staticmethod
+    def print_class_info(cls):
+        print(f"Class: {cls.__name__}\n")
+
+        print("🔧 Attributes:")
+        for name, value in cls.__dict__.items():
+            if not inspect.isroutine(value):
+                print(f"  {name} = {value}")
+
+        print("\n📦 Methods:")
+        for name, value in cls.__dict__.items():
+            if inspect.isroutine(value):
+                print(f"  {name}()")
+
+    #PDF CRUD
+    # @staticmethod
+    # def get_pdf_text(path:str):
+    
+    #     doc = fitz.open(path)
+    #     text_data = {}
+    #     for pgn in range(doc.page_count):
+    #         page = doc[pgn]
+    #         text = page.get_text("text")
+    #         text = text.encode('utf-8', 'ignore').decode('utf-8')
+    #         data = text.split('\n')
+    #         text_data [pgn] = data
+    #     return text_data
+    
+    # @staticmethod
+    # def get_clipped_data(input:str, bboxes:list[set]):
+    
+    #     document = fitz.open(input)
+    #     final_list = []
+        
+    #     for pgn in range(document.page_count):
+    #         page = document[pgn]
+
+    #         blocks = []
+    #         for bbox in bboxes:
+    #             blocks.extend(page.get_text('dict', clip = bbox)['blocks']) #get all blocks
+            
+    #         filtered_blocks = [block for block in blocks if block['type']== 0 and 'lines' in block]
+    #         sorted_blocks = sorted(filtered_blocks, key= lambda x: (x['bbox'][1], x['bbox'][0]))
+            
+    #         final_list.append({
+    #         "pgn": pgn,
+    #         "block": sorted_blocks
+    #         })
+            
+            
+    #     document.close()
+    #     return final_list
+    
+    # @staticmethod
+    # def get_all_pdf_data(path:str):
+    
+    #     doc = fitz.open(path)
+    #     count = doc.page_count
+    #     all_blocks = list()
+
+    #     for pgn in range(count):
+    #         page = doc[pgn]
+            
+    #         blocks = page.get_text('dict')['blocks']
+    #         images = page.get_images()
+    #         filtered_blocks = [block for block in blocks if block['type']== 0]
+    #         sorted_blocks = sorted(filtered_blocks, key=lambda x: x['bbox'][1])
+    #         all_blocks.append({
+    #             "pgn":pgn,
+    #             "blocks":sorted_blocks,
+    #             "images": images
+    #         })
+            
+    #         #draw lines
+            
+    #         lines = fitz.Rect()
+            
+    #     doc.close()
+        
+    #     return all_blocks
+    
+    
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import json5, requests,os
 from datetime import datetime
 from app.program_logger import setup_logger
 from app.action_executor import ActionExecutor
+from app.utils import Helper
 from selenium.webdriver.chrome.options import Options
 # from selenium.webdriver.chrome.webdriver import WebDriver
 
@@ -15,68 +16,61 @@ with open(r"paths.json") as f:
 
 #constants
 BANK_CODES = ["PSB_1","PSB_2","PSB_3","PSB_4","PSB_5","PSB_6","PSB_7","PSB_8","PSB_9","PSB_10","PSB_11","PSB_12"] #"PSB_1","PSB_2","PSB_3","PSB_6","PSB_7","PSB_8","PSB_9","PSB_10","PSB_11","PSB_12"
-BANK_CODES = ["PSB_3"]
-LOG_DIR = os.path.join(paths["output"],paths["folders"]["log"])
-CACHE_DIR = os.path.join(paths["output"],paths["folders"]["cache"])
+# BANK_CODES = ["PSB_3"]
+LOG_DIR = Helper.create_dirs(paths["output"],[paths["folders"]["log"]])
+CACHE_DIR = Helper.create_dirs(paths["output"],[paths["folders"]["cache"]])
 TODAY = datetime.now().strftime("%d%m%Y")
 
-os.makedirs(LOG_DIR,exist_ok=True)
-os.makedirs(CACHE_DIR,exist_ok=True)
 
 logger = setup_logger(name="scraper",log_dir=LOG_DIR)
-final_dict = {
-    "metadata":{
-        "program":"main.py",
-        "timestamp":None,
-        "config":"params.json5"
-    },
-    "records":[]
-    
-}
+final_dict = {"metadata":{"filename":f"up{TODAY}.json","program":"main.py","timestamp":TODAY,"config":"params.json5"},"records":[]}
 
 #selenium
-options = Options()
-driver = webdriver.Chrome(options=options)
-
 for code in BANK_CODES:
+    
     WEBSITE = config[code]
+    logger.info(f"Scraping From Bank {WEBSITE['bank_name']}")
     website = WEBSITE["base_url"]
-    
-    scrape_data = {"bank_name": WEBSITE["bank_name"],"base_url":WEBSITE["base_url"],"response": {}}
-    
-    logger.info(f"Scraping From Bank {WEBSITE["bank_name"]}")
-    executor = ActionExecutor(driver, logger,WEBSITE,paths)
+    scrape_data = {"bank_name": WEBSITE["bank_name"], "base_url": website, "response": {}}
+
+    # Create a fresh driver per bank
+    options = Options()
+    # options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(options=options)
 
     try:
-        resp = requests.get(website, headers=WEBSITE["headers"], timeout=5, verify=False)
+        session = requests.Session()
+        session.headers.update(WEBSITE["headers"])
+        timeout = 120 if code in ["PSB_12", "PSB_7"] else 25
+        verify_ssl = WEBSITE.get("verify_ssl", False)  # configurable per bank
+        resp = session.get(website, timeout=timeout, verify=verify_ssl)
+
         if resp.status_code not in [200, 201, 202, 203, 204, 206, 301, 302, 303, 307, 308]:
             logger.warning(f"{website} returned status: {resp.status_code}")
             raise Exception(f"{resp.status_code}")
-        
-        executor.attach_headers(driver,WEBSITE["headers"])
+
+        executor = ActionExecutor(driver, logger, WEBSITE, paths)
+        executor.attach_headers(driver, WEBSITE)
         driver.get(website)
-        logger.notice("Page fetched successfully")
-        
+        logger.info("Page fetched successfully")
+
+        for idx, block in enumerate(WEBSITE["blocks"]):
+            logger.info(f"Running Block: {idx}")
+            data, timestamp = executor.execute_blocks(block)
+            scrape_data["response"].update(data)
+
     except Exception as e:
-        logger.error(f"Requests error: {e}")
-        logger.notice(f"Skipping for {WEBSITE["bank_name"]}")
+        logger.error(f"Error scraping {WEBSITE['bank_name']}: {e}")
         scrape_data["response"] = {"error": str(e)}
-        final_dict["records"].append(scrape_data)
-        
-        continue
-    
-    for idx,block in enumerate(WEBSITE["blocks"]):
-        print(f"Running Block: {idx}")
-        data,timestamp = executor.execute_blocks(block)
-        # scrape_data["response"].update(
-        #     {f"block_{idx+1}":{
-        #     "timestamp":timestamp,
-        #     "scrape_data":data
-        # }})
-        scrape_data["response"].update(data)
-    
+
+    finally:
+        driver.quit()
+
     final_dict["records"].append(scrape_data)
     
-with open(os.path.join(CACHE_DIR,f"cache_{TODAY}.json5"),"w+") as f:
-    json5.dump(final_dict,f)
-driver.quit()
+
+Helper.save_json(final_dict,os.path.join(CACHE_DIR,f"cache_{TODAY}.json5"))
